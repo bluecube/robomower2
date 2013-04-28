cb5b77f12f9339afe7f5f11b4fac072061561d69
Kuba Marek <blue.cube@seznam.cz>
2013-04-28 15:38:23 +0200

    TWI transfers -- roughly.

--- src/drive/main.c
+++ src/drive/main.c
@@ -1,18 +1,18 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/twi.h>
 #include <stdint.h>
 
 #if F_CPU != 8000000L
 #error This will work only for 8MHz CPU frequency
 #endif
 
-#ifndef I2C_ADDRESS
-#error I2C address must be set!
-#endif
-
 #define SERVO_PRESCALER 8
 #define SERVO_TICKS_PER_MS ((uint16_t)(F_CPU / (SERVO_PRESCALER * (uint16_t)1000)))
 #define SERVO_NEUTRAL_POSITION ((3 * SERVO_TICKS_PER_MS) / 2)
 #define SERVO_PERIOD (20 * SERVO_TICKS_PER_MS)
+
+#define SENSOR_ENABLED (PORTD & _BV(PD3))
 
 void sensor_enable()
 {
@@ -26,15 +26,9 @@ void sensor_disable()
     PORTD &= ~_BV(PD4); // disable pull up resistor
 }
 
-/// Set the output value for the servo as signed integer from -127 to 127.
-void servo_set(int16_t value)
+void servo_enable()
 {
-    OCR1A = SERVO_NEUTRAL_POSITION + (value * (SERVO_TICKS_PER_MS / 2)) / 127;
-}
-
-void servo_enable(int16_t value)
-{
-    servo_set(value);
+    sensor_enable();
 
     TCNT1 = SERVO_PERIOD - 1; // We want a complete period of the servo to happen --
                               // including the first rising edge.
@@ -47,6 +41,24 @@ void servo_disable()
 {
     TCCR1B &= _BV(CS10) | _BV(CS11) | _BV(CS12);
     PORTB &= ~_BV(PB1); // Set the output pin to zero.
+
+    sensor_disable();
+}
+
+/// Set the output value for the servo as signed integer from -127 to 127.
+/// Value of -128 means disabling of the servo.
+void servo_set(int16_t value)
+{
+    if (value == -128)
+    {
+        servo_disable();
+        return;
+    }
+
+    if (!SENSOR_ENABLED)
+        servo_enable();
+
+    OCR1A = SERVO_NEUTRAL_POSITION + (value * (SERVO_TICKS_PER_MS / 2)) / 127;
 }
 
 void setup()
@@ -73,8 +85,9 @@ void setup()
         _BV(TWEA) | // Enable acknowledge
         _BV(TWEN) | // enable
         _BV(TWIE); // enable interrupts
-}
 
+    sei();
+}
 
 /// Return the measured distance in ticks, or
 /// UINT8_MAX to indicate overflow.
@@ -96,3 +109,23 @@ uint8_t distance_travelled()
     }
 }
 
+int main()
+{
+    setup();
+    while(1);
+}
+
+ISR(TWI_vect)
+{
+    switch(TW_STATUS)
+    {
+    case TW_SR_DATA_ACK:
+        servo_set(TWDR);
+        break;
+    case TW_ST_SLA_ACK:
+        TWDR = distance_travelled();
+        break;
+    default:
+        break;
+    }
+}
