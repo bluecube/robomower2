9b895ecb185ecafac385f82c6eacfe7956f2f966
Kuba Marek <blue.cube@seznam.cz>
2013-08-29 18:44:50 +0200

    WIP drive board software.

--- src/drive/main.c
+++ src/drive/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/twi.h>
 #include <stdint.h>
+#include <robonet/robonet.h>
 
 #if F_CPU != 8000000L
 #error This will work only for 8MHz CPU frequency
@@ -12,29 +12,16 @@
 #define SERVO_NEUTRAL_POSITION ((3 * SERVO_TICKS_PER_MS) / 2)
 #define SERVO_PERIOD (20 * SERVO_TICKS_PER_MS)
 
-#define SENSOR_ENABLED (PORTD & _BV(PD3))
-
 /// Calculate ((a << shift) / b) with correct rounding
 /// a << (shift + 1) must fit into uint32_t
 #define ROUNDED_DIVISION(a, b, shift) \
     (((((uint32_t)(a)) << ((shift) + 1)) / (b) + 1) >> 1)
 
+uint16_t latchedTicks;
 
-void sensor_enable()
+void motor_enable()
 {
-    PORTD |= _BV(PD3); // enable sensor power
-    PORTD |= _BV(PD4); // enable pull up resistor
-}
-
-void sensor_disable()
-{
-    PORTD &= ~_BV(PD3); // disable sensor power
-    PORTD &= ~_BV(PD4); // disable pull up resistor
-}
-
-void servo_enable()
-{
-    sensor_enable();
+    PORTD |= _BV(PD4); // Enable sensor power
 
     TCNT1 = SERVO_PERIOD - 1; // We want a complete period of the servo to happen --
                               // including the first rising edge.
@@ -43,28 +30,19 @@ void servo_enable()
         // setting the prescaler to 8 => enabling timer.
 }
 
-void servo_disable()
+void motor_disable()
 {
     TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
         // Clear the prescaler flags => disable timer
     PORTB &= ~_BV(PB1); // Set the output pin to zero.
 
-    sensor_disable();
+    PORTD &= ~_BV(PD4); // disable sensor power
 }
 
 /// Set the output value for the servo as signed integer from -127 to 127.
 /// Value of -128 means disabling of the servo.
 void servo_set(int8_t value)
 {
-    if (value == -128)
-    {
-        servo_disable();
-        return;
-    }
-
-    if (!SENSOR_ENABLED)
-        servo_enable();
-
     // OCR1A = SERVO_NEUTRAL_POSITION +
     //    (value * (SERVO_TICKS_PER_MS / 2)) / 127;
     uint16_t multiplier = ROUNDED_DIVISION(SERVO_TICKS_PER_MS / 2, INT8_MAX, 8);
@@ -91,11 +69,11 @@ uint8_t distance_travelled()
     }
 }
 
-int main()
+void init()
 {
     // Output ports:
     DDRB |= _BV(PB1); // servo
-    DDRD |= _BV(PD3); // snsor power
+    DDRD |= _BV(PD4); // sensor power
 
     // Timer0 (counting encoder pulses)
     TCCR0 |= _BV(CS02) | _BV(CS01) | _BV(CS00); // external clock source, rising
@@ -109,29 +87,40 @@ int main()
         // Timer disabled. The prescaler is set when servo output is enabled
     ICR1 = SERVO_PERIOD - 1; // setting the top value to 20ms
 
-    // I2C communication
-    TWAR = I2C_ADDRESS << 1;
-    TWCR =
-        _BV(TWEA) | // Enable acknowledge
-        _BV(TWEN) | // enable
-        _BV(TWIE); // enable interrupts
-
-    sei(); // bzzzzzzzzz........
-
-    while(1);
+    robonet_init();
 }
 
-ISR(TWI_vect)
+int main()
 {
-    switch(TW_STATUS)
+    init();
+    sei(); // bzzzzzzzzz........
+
+    while(true)
     {
-    case TW_SR_DATA_ACK:
-        servo_set(TWDR);
-        break;
-    case TW_ST_SLA_ACK:
-        TWDR = distance_travelled();
-        break;
-    default:
-        break;
+        uint8_t status = robonet_receive();
+        if (status == ROBONET_BUSY)
+            continue;
+
+        if (robonetBuffer.address == 0x0f)
+        {
+            // broadcast function 0x00: latch
+
+            // TODO
+
+            continue; // don't transmit anything
+        }
+        else if (robonetBuffer.address == 0x00 | ROBONET_OWN_ADDRESS)
+        {
+            // addressed function 0x00: order new speed and transmit latched value
+            uint8_t orderedValue = robonetBuffer.data[0];
+
+            *((uint16_t)robonetBuffer.data) = latchedTicks;
+            robonetBuffer.length = 2;
+        }
+
+        // We're transmitting something, do the common stuff here
+        robonetBuffer.address = 0x00;
+        // TODO: Append the error status if it is nonzero
+        robonet_transmit();
     }
 }
