c9764a4f21e89e63ec4d3508d826c6ee767c09fd
Kuba Marek <blue.cube@seznam.cz>
2013-10-27 11:43:44 +0100

    Partially syncing software to the real board.

--- test/echo/test.py
+++ test/echo/test.py
@@ -2,6 +2,6 @@
 from src_python import robonet
 
 r = robonet.RoboNet('/dev/ttyUSB1', 38400)
-answer = r.send_message(robonet.RoboNetPacket(1, bytes(range(10)))
+answer = r.send_message(robonet.RoboNetPacket(1, bytes(range(10))))
 print("echo: " + str([hex(x) for x in answer.data[:-1]]))
 print("status: " + hex(answer.data[-1]))
