ca9ca45cb8f62229ced04a5d6f0cc1db1602c7c5
Kuba Marek <blue.cube@seznam.cz>
2013-11-09 10:00:42 +0100

    Solved the problem with atomic reads of a counter extended in software.

--- src_avr/drive/main.c
+++ src_avr/drive/main.c
@@ -1,31 +1,53 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/atomic.h>
 #include <stdint.h>
 #include "servo/servo.h"
 #include "build/drive.interface.h"
 
-uint16_t latchedTicks;
-uint8_t ticksHigh;
+#define lo8(x) (((uint8_t*)&(x))[0])
+#define hi8(x) (((uint8_t*)&(x))[1])
 
-/// Return the measured distance in ticks, or
-/// UINT8_MAX to indicate overflow.
-/// Also reset the counter and clear the overflow flag.
-uint8_t distance_travelled()
+struct latched_value
 {
-    uint8_t tmp = TCNT0;
-    TCNT0 = 0;
+    uint16_t state;
+    uint16_t previousState;
+};
 
-    if (TIFR & _BV(TOV0))
+struct latched_value odometryTicks;
+
+volatile uint8_t ticksHigh;
+
+/** Atomically read both lower and upper (extended by software) part of the encoder
+ * tick counter and update the struct latched_value.
+ * This function assumes that it finishes faster than is the period between encoder ticks
+ * (at most one encoder tick may happen inside this function) and that it is executed with
+ * interrupts enabled (because the counter high word is updated in an interrupt).
+ * If these conditions are not met, then atomicity is lost and the low and high
+ * bytes of the value might be desynchronized.*/
+static void latch_encoder_ticks(struct latched_value* out)
+{
+    uint8_t tmpTicks = TCNT0;
+    uint8_t tmpTicksHigh = ticksHigh;
+
+    if (tmpTicks > TCNT0)
     {
-        // If there was an overflow
-        TIFR |= _BV(TOV0); // Clearing flag by writing 1 into it? Datasheet seems to say so.
-        return UINT8_MAX;
+        // there has been a timer overflow at timer 0
+        // and it might have occured between reading TCNT0 and reading ticksHigh
+        // we need to do it all over again.
+        // The same problem will not repeat, because this function should execute
+        // faster than a period between input ticks
+        //
+        // If there was only a single tick without overflow, we ignore it
+        // and declare that the read happened before it (which is correct behavior).
+        tmpTicks = TCNT0;
+        tmpTicksHigh = ticksHigh;
     }
-    else
-    {
-        return tmp;
-    }
+
+    // Now everything is safe in tmpTicks and tmpTicksHigh!
+
+    out->previousState = out->state;
+    lo8(out->state) = tmpTicks;
+    hi8(out->state) = tmpTicksHigh;
 }
 
 int main()
@@ -47,18 +69,12 @@ void handle_update_request(const struct update_request* in,
                            struct update_response* out)
 {
     servo_set(in->speed);
-    out->distance = latchedTicks;
+    out->distance = odometryTicks.state - odometryTicks.previousState;
 }
 
 void handle_latch_values_broadcast()
 {
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-    {
-        // Race condition!
-        latchedTicks = TCNT0 | (ticksHigh << 8);
-        TCNT0 = 0;
-        ticksHigh = 0;
-    }
+    latch_encoder_ticks(&odometryTicks);
 }
 
 ISR(TIMER0_OVF_vect)
