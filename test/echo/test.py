c4e6f8799593e1bb1e8c90f68fdd03d0d62bcb15
Kuba Marek <blue.cube@seznam.cz>
2013-11-13 11:02:47 +0100

    More verbose echo test.

--- test/echo/test.py
+++ test/echo/test.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from src_python import robonet
 
+def hexify(data):
+    return str([hex(x) for x in data])
+
 r = robonet.RoboNet('/dev/ttyUSB1', 38400)
-answer = r.send_message(robonet.RoboNetPacket(1, bytes(range(10))))
-print("echo: " + str([hex(x) for x in answer.data[:-1]]))
+packet = robonet.RoboNetPacket(1, bytes(range(2)))
+print("data: " + hexify(bytes(packet.data)))
+print("seding: " + hexify(bytes(packet)))
+answer = r.send_message(packet)
+print("echo: " + hexify(answer.data[:-1]))
 print("status: " + hex(answer.data[-1]))
