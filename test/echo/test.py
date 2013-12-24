0896ae50799ade220ca609a51f49044a01e7c16e
Kuba Marek <blue.cube@seznam.cz>
2013-12-24 02:09:03 +0100

    Longer echo test data.

--- test/echo/test.py
+++ test/echo/test.py
@@ -5,7 +5,7 @@ def hexify(data):
     return str([hex(x) for x in data])
 
 r = robonet.RoboNet('/dev/ttyUSB1', 38400)
-packet = robonet.RoboNetPacket(1, bytes(range(2)))
+packet = robonet.RoboNetPacket(1, bytes(range(3)))
 print("data: " + hexify(bytes(packet.data)))
 print("seding: " + hexify(bytes(packet)))
 answer = r.send_message(packet)
