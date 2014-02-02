bf4212ac904e6ee77fd25d83123064113a7f9ea9
Kuba Marek <blue.cube@seznam.cz>
2014-02-02 11:28:23 +0100

    Working calibration code for turningy ESC.

--- src_avr/drive/turnigy.c
+++ src_avr/drive/turnigy.c
@@ -3,19 +3,16 @@
 
 #include <util/delay.h>
 
-void delay()
-{
-    for (uint16_t i = 0; i < 1000; ++i)
-        _delay_ms(1);
-}
-
 __attribute__ ((noinline, cold))
 void calibrate_turningy_esc()
 {
     servo_set16(SERVO_RANGE_TICKS);
-    delay();
-    delay();
+    for (uint16_t i = 0; i < 10000; ++i)
+        _delay_ms(1);
     servo_set16(-SERVO_RANGE_TICKS);
-    delay();
+    for (uint16_t i = 0; i < 10000; ++i)
+        _delay_ms(1);
     servo_set16(0);
+    for (uint16_t i = 0; i < 10000; ++i)
+        _delay_ms(1);
 }
