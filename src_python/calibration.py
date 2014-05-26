ea8ae9ce041b323904028d28f22d2580e4926411
Kuba Marek <blue.cube@seznam.cz>
2014-05-26 09:35:18 +0200

    Preparing calibration code for stdev estimation.

--- src_python/calibration.py
+++ src_python/calibration.py
@@ -42,47 +42,44 @@ def squaredDistanceFromLines(p, lines):
 def loadRecording(path):
     return [x[2:4] for x in datalogger.load(path) if x[2] != 0 or x[3] != 0]
 
-def objective(ticks, ground_truth, state_vector):
-    """ Objective function to minimize.
-    First two arguments contain the driven path and ground truth, rest are
-    state vector elements.
-    Returns sum of squared distances from the ground truth over the whole path. """
+def optimize_dimensions(ticks, ground_truth, left_resolution, right_resolution, wheel_base, **kwargs):
+    """Find estimate of wheel distances per tick and wheel base dimensions.
+    First two arguments contain the driven path and ground truth, resolutions and
+    wheel base are the initial estimates."""
 
-    x, y, heading, left_resolution, right_resolution, wheel_base = state_vector
+    def objective(state_vector):
+        """Returns sum of squared distances from the ground truth over the whole path."""
+        x, y, heading, left_resolution, right_resolution, wheel_base = state_vector
 
-    class Sample:
-        pass
-    initialSample = Sample()
-    initialSample.x = x
-    initialSample.y = y
-    initialSample.heading = heading
+        class Sample:
+            pass
+        initialSample = Sample()
+        initialSample.x = x
+        initialSample.y = y
+        initialSample.heading = heading
 
-    drive = differential_drive.DifferentialDriveModel(left_resolution,
-                                                      right_resolution,
-                                                      wheel_base)
+        drive = differential_drive.DifferentialDriveModel(left_resolution,
+                                                          right_resolution,
+                                                          0, 0,
+                                                          wheel_base)
 
-    return squaredDistanceFromLines((x, y), ground_truth) + \
-           math.fsum(squaredDistanceFromLines((sample.x, sample.y), ground_truth)
-                     for sample in
-                     drive.update_sample_iter(initialSample, ticks))
+        return squaredDistanceFromLines((x, y), ground_truth) + \
+               math.fsum(squaredDistanceFromLines((sample.x, sample.y), ground_truth)
+                         for sample in
+                         drive.update_sample_iter(initialSample, ticks))
 
-def optimize(ticks, ground_truth, left_resolution, right_resolution, wheel_base, **kwargs):
-    result = scipy.optimize.minimize(functools.partial(objective, ticks, ground_truth),
+    result = scipy.optimize.minimize(objective,
                                      [0, 0, 0, # Initial position and orientation
                                       left_resolution, right_resolution, wheel_base],
                                      options = {"disp": True, "maxiter": 1000},
                                      method = "TNC",
                                      bounds = [(-1, 1), (-1, 1), (-2, 2),
-                                                (0.1 * left_resolution, 10 * left_resolution),
-                                                (0.1 * right_resolution, 10 * right_resolution),
-                                                (0.1 * wheel_base, 10 * wheel_base)])
-    return result
+                                               (0.1 * left_resolution, 10 * left_resolution),
+                                               (0.1 * right_resolution, 10 * right_resolution),
+                                               (0.1 * wheel_base, 10 * wheel_base)])
 
-def c(x):
-    return math.cos(math.radians(x))
+    return result['x']
 
-def s(x):
-    return math.sin(math.radians(x))
 
 # The shape for calibration
 #
@@ -94,8 +91,8 @@ def s(x):
 
 p1 = (0, 0)
 p2 = (5, 0)
-p3 = (5 * c(60), 5 * s(60))
-p4 = (5 + 5 * c(60), 5 * s(60))
+p3 = (5 * math.cos(math.radians(60)), 5 * math.sin(math.radians(60)))
+p4 = (5 + p3[0], p3[1])
 
 ground_truth = [
     (p1, p2),
@@ -104,7 +101,6 @@ ground_truth = [
     (p1, p3),
     (p2, p4)]
 
-
 if __name__ == "__main__":
     import json
     import sys
@@ -112,6 +108,14 @@ if __name__ == "__main__":
     with open("config.json", "r") as fp:
         config = json.load(fp)
 
+    drive_config = config["drive"]
+
     recording = loadRecording(sys.argv[1])
 
-    print(optimize(recording, ground_truth, **config["drive"]))
+    x0, y0, heading0, \
+        drive_config["left_resolution"], \
+        drive_config["right_resolution"], \
+        drive_config["wheel_base"] = \
+        optimize_dimensions(recording, ground_truth, **drive_config)
+
+    print(json.dumps(drive_config, indent=4, sort_keys=True))
