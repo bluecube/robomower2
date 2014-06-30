bd64d87819b0159ab80a57e18334ea1490e3ac31
Kuba Marek <blue.cube@seznam.cz>
2014-06-30 17:02:22 +0200

    Rough gui showing calibration progress.

--- src_python/calibration.py
+++ src_python/calibration.py
@@ -4,11 +4,62 @@ import math
 import functools
 import scipy.optimize
 import numpy
+import pygame
+import gui.config
+import gui.widgets
+
 
 import differential_drive
 import datalogger
 
 import util.frechet_distance
+
+class CalibrationGui:
+    pygame.init()
+
+    def __init__(self, ground_truth):
+        self._screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+        self._logwidget = gui.widgets.LogWidget(False)
+        self._ground_truth = ground_truth
+        self._prev_path = []
+
+    def _draw_path(self, path, color):
+        if not len(path):
+            return
+
+        w, h = self._screen.get_size()
+        cx = w / 2
+        cy = h / 2
+        scale = 0.8 * min(w / (2 * p4[0]), h / (2 * p4[1]))
+
+        prev_x = cx + scale * path[0][0]
+        prev_y = cy - scale * path[0][1]
+
+        for p in path[1:]:
+            x = cx + scale * p[0]
+            y = cy - scale * p[1]
+
+            pygame.draw.line(self._screen, color, (prev_x, prev_y), (x, y))
+            prev_x = x
+            prev_y = y
+
+    def update(self, state_vector, dist, path):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.finished = True
+                return
+            elif event.type == pygame.VIDEORESIZE:
+                self._screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
+        self._screen.fill(gui.config.bgcolor)
+        self._logwidget.add_line(", ".join("{:.4e}".format(param) for param in state_vector) + " => {:.4e}".format(dist))
+        w, h = self._screen.get_size()
+        self._logwidget.draw(self._screen, 0, 0, w, h, None)
+        self._draw_path(self._ground_truth, gui.config.color2)
+        self._draw_path(self._prev_path, gui.config.color1_50)
+        self._draw_path(path, gui.config.color1)
+        pygame.display.flip()
+        self._prev_path = path
+
 
 def loadRecording(path):
     return [x[3:5] for x in datalogger.load(path) if x[3] != 0 or x[4] != 0]
@@ -21,7 +72,6 @@ def optimize_dimensions(ticks, ground_truth, left_resolution, right_resolution, 
     def objective(state_vector):
         """Returns sum of squared distances from the ground truth over the whole path."""
         x, y, heading, left_resolution, right_resolution, wheel_base = state_vector
-        print(x, y, heading, left_resolution, right_resolution, wheel_base)
 
         avg_tick_size = (left_resolution + right_resolution) / 2
 
@@ -40,12 +90,15 @@ def optimize_dimensions(ticks, ground_truth, left_resolution, right_resolution, 
                 for sample in
                 drive.update_sample_iter(initialSample, ticks)]
 
+
         dist = util.frechet_distance.frechet_distance(path, ground_truth)
-        print("=>", dist)
+        calibrationGui.update(state_vector, dist, path)
         return dist
 
     initial_state = [0, 0, 0, # Initial position and orientation
                      left_resolution, right_resolution, wheel_base]
+
+    calibrationGui.update(initial_state, -1, [])
 
     result = scipy.optimize.minimize(objective, initial_state,
                                      options = {"disp": True, "maxiter": 1000},
@@ -128,6 +181,8 @@ if __name__ == "__main__":
     #drive_config["left_resolution"] *= 1.014
     #drive_config["right_resolution"] *= 0.99123
     #drive_config["wheel_base"] *= 1.09123
+
+    calibrationGui = CalibrationGui(ground_truth)
 
     print("before")
     print(json.dumps(drive_config, indent=4, sort_keys=True))
