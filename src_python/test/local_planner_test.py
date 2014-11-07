a60ee89f528159327657723113dac283dfb94237
Kuba Marek <blue.cube@seznam.cz>
2014-11-07 20:12:49 +0100

    Refactoring in local planner test.

--- src_python/test/local_planner_test.py
+++ src_python/test/local_planner_test.py
@@ -4,19 +4,18 @@ import math
 from path_planning import local_planner
 from path_planning import State
 
+def check_it_equal_to_state(it, state):
+    for fieldname in State._fields:
+        assert_almost_equal(getattr(it, fieldname), getattr(state, fieldname), msg="Field " + fieldname)
+
 def check_path(state1, state2):
     it = local_planner.plan_path(state1, state2)
 
-    for fieldname in State._fields:
-        assert_almost_equal(getattr(it, fieldname), getattr(state1, fieldname), msg="Field " + fieldname)
-
+    check_it_equal_to_state(it, state1)
     it.advance(it.travel_time)
-    for fieldname in State._fields:
-        assert_almost_equal(getattr(it, fieldname), getattr(state2, fieldname), msg="Field " + fieldname)
-
+    check_it_equal_to_state(it, state2)
     it.reset()
-    for fieldname in State._fields:
-        assert_almost_equal(getattr(it, fieldname), getattr(state1, fieldname), msg="Field " + fieldname)
+    check_it_equal_to_state(it, state1)
 
     n = 1000
     prev_x = it.x
@@ -36,9 +35,7 @@ def check_path(state1, state2):
         prev_y = it.y
         prev_v = it.velocity
 
-    for fieldname in State._fields:
-        assert_almost_equal(getattr(it, fieldname), getattr(state2, fieldname), msg="Field " + fieldname)
-
+    check_it_equal_to_state(it, state2)
 
 def test():
     check_path(State(0, 0, 0, 1, 0, 0),
