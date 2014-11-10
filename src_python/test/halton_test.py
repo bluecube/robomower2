8073dabdf3f653891a75933f146b0e7fb85c2866
Kuba Marek <blue.cube@seznam.cz>
2014-11-10 08:03:15 +0100

    Checking the values of halton sequence.

--- src_python/test/halton_test.py
+++ src_python/test/halton_test.py
@@ -14,3 +14,12 @@ def uniqueness_test():
     for i in range(1000):
         values.add(tuple(next(sequence)))
     assert_equal(len(values), 1000)
+
+def values_test():
+    it = halton.HaltonSequence(2)
+    for expected in [(1/2, 1/3), (1/4, 2/3), (3/4, 1/9), (1/8, 4/9), (5/8, 7/9), (3/8, 2/9)]:
+        actual = next(it)
+
+        print("actual = {}, expected = {}".format(str(actual), str(expected)))
+        assert_almost_equal(actual[0], expected[0])
+        assert_almost_equal(actual[1], expected[1])
